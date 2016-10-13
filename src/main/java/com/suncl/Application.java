ede5463c5e33cb4b25f274372a64716e3a9a32e5
suncl <suncl@ios.ac.cn>
2016-10-13 09:12:02 +0800

    ADD: JdbcTemplate

---
+++ src/main/java/com/suncl/Application.java
@@ -0,0 +1,73 @@
+package com.suncl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.dbcp")
+    public DataSource mysqlSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JdbcTemplate mysqlJdbcTemplate() {
+        return new JdbcTemplate(mysqlSource());
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        JdbcTemplate jdbcTemplate = mysqlJdbcTemplate();
+        testH2(jdbcTemplate);
+    }
+
+
+    public void testH2(JdbcTemplate jdbcTemplate) {
+
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE  IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[]{"Josh"},
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> log.info(customer.toString()));
+    }
+}
--- src/main/java/com/suncl/MainApp.java
+++ src/main/java/com/suncl/MainApp.java
@@ -7,11 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by Chenglong Sun on 2016/9/21.
  */
 public class MainApp {
-
+/*
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
         HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloWorld");
         helloWorld.printMessage();
     }
-
+*/
 }
